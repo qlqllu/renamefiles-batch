c9080372e0e8756026f37220b956b8bdb01c7ad3
Mark Liu <qlqllu@126.com>
2013-08-11 16:29:39 +0800

    init

--- README.md
+++ README.md
@@ -2,3 +2,16 @@ renamefiles-batch
 =================
 
 rename files batch by a given txt file
+
+-----------------
+Intall:
+npm intall -g renamefiles-batch
+-----------------
+How to use:
+# cd the folder where you want to rename the files. The folder should have:
+## A txt file which name is list.txt, the file format is like this:
+   [folder name]
+   file1 name
+   file2 name
+# Run renamefiles-batch
+# Please note that the file order is very important!
---
+++ app.js
@@ -0,0 +1,46 @@
+var fs = require('fs'),
+path = require('path');
+
+var arguments = process.argv.splice(2), dir;
+if(arguments.length >= 1){
+  dir = arguments[0];
+}else{
+  dir = '.';
+}
+
+var data = fs.readFileSync(dir + path.sep + 'list.txt', {encoding: 'utf-8'});
+var lines = data.split('\r\n'), i, dirName, fileName, fileIndex, files;
+
+console.log('List file line count: ' + lines.length);
+
+for(i = 0; i < lines.length; i++){
+  if(lines[i].trim().length === 0){
+    continue;
+  }
+  var m = lines[i].match(/\[(.*)\]/);
+  if(m){
+    dirName = dir + path.sep + m[1];
+    fileIndex = 0;
+    if(fs.existsSync(dirName)){
+      files = fs.readdirSync(dirName);
+    }else{
+      files = [];
+    }
+    console.log('Read dir ' + dirName + ', file count: ' + files.length);
+  }else{
+    fileName = dirName + path.sep + lines[i];
+    if(fs.existsSync(dirName + path.sep + files[fileIndex])){
+      var oldName = dirName + path.sep + files[fileIndex],
+      p = oldName.lastIndexOf('.'), ext;
+      if(p < 0){
+        ext = '';
+      }else{
+        ext = oldName.substring(p, oldName.length);   
+      }
+      
+      fs.renameSync(oldName, fileName + ext);
+      console.log('Rename: ' + files[fileIndex] + '->' + fileName);
+    }
+    fileIndex ++;
+  }
+}
